79a610dfa168dc9c0d9896719cd1aacb6c3de795
codexmish <majharulislam.shanto.70@gmail.com>
2026-05-12 15:50:06 +0600

    pass bycript added

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,36 +1,61 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVeified: {
+      type: Boolean,
+    },
+    avatar: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    otp: {
+      type: String,
+      default: null,
+    },
+    otpExpiry: {
+      type: Date,
+    },
+    role: {
+      type: String,
+      require: true,
+      default: "user",
+      enum: ["user", "moderator", "admin"],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  otp: {
-    type: String,
-    default: null,
-  },
-  otpExpiry: {
-    type: Date,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  try {
+    const round = 10;
+    const salt = await bcrypt.genSalt(round);
+    this.password = await bcrypt.hash(this.password, salt);
+  } catch (error) {
+    res.status(500).send({ success: false, message: "Internal server Error" });
+  }
 });
+
+userSchema.methods.comparePassword = async function (plainPass) {
+  return await bcrypt.compare(plainPass, this.password);
+};
 
 module.exports = mongoose.model("user", userSchema);
